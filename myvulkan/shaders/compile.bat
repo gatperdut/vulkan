c08bde3a20e714f6740b6be0dfcb985af1cf43ba
gatperdut <carlos.romero.breto@gmail.com>
2018-10-28 15:53:16 +0100

    initial

---
+++ myvulkan/shaders/compile.bat
@@ -0,0 +1,4 @@
+C:\VulkanSDK\1.1.85.0\Bin32\glslangValidator.exe -V C:\Users\gatperdut\source\repos\myvulkan\myvulkan\shaders/shader.vert
+C:\VulkanSDK\1.1.85.0\Bin32\glslangValidator.exe -V C:\Users\gatperdut\source\repos\myvulkan\myvulkan\shaders/shader.frag
+pause
+exit
